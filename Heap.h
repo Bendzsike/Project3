f819296470f84a1c64cda7b482bc4b649078f11d
Imola <imolci01@gmail.com>
2021-05-24 16:20:56 +0300

    Added Heap to the project.

---
+++ Heap.h
@@ -0,0 +1,24 @@
+//
+// Created by Imola on 5/24/2021.
+//
+
+#ifndef PROJECT_3_HEAP_H
+#define PROJECT_3_HEAP_H
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int size, *elements;
+}HEAP;
+
+void Create(HEAP *heap);
+void Insert(HEAP *heap, int key);
+void Lift(HEAP *heap, int i);
+void Swap(int *a, int *b);
+int Top(HEAP heap);
+void PrintHeap(HEAP heap);
+void Delete(HEAP *heap);
+void IncreaseDataAtIndex(HEAP* mH, int index, int value);
+void MergeHeaps(HEAP* h1, HEAP* h2);
+
+#endif //PROJECT_3_HEAP_H
